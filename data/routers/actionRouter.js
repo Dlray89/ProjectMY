dc8bf5e82d99ba45d6162822f6563f8df0588c65
Dlray89 <dlrayjr89@gmail.com>
2020-03-13 11:59:00 -0400

    action route is done and it works

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -16,4 +16,40 @@ router.get("/:id", (req,res) => {
     })
 })
 
+router.post("/", (req,res) => {
+    const newAction = req.body
+    actionDB
+    .insert(newAction)
+    .then(newAction => {
+        res.status(200).json(newAction)
+    })
+    .catch(error => {
+        res.status(500).json({errorMessage: `${error} Could not post to actions`})
+    })
+})
+
+router.put("/:id", (req,res) => {
+    const { id } = req.params
+    const updateActions = req.body
+    actionDB
+    .update(id, updateActions)
+    .then(updateActions => {
+        res.status(200).json(updateActions)
+    })
+    .catch(error => {
+        res.status(500).json({errorMessage: `${error} Could not update your actions`})
+    })
+})
+
+router.delete("/:id", (req,res) => {
+    const { id } = req.params
+    actionDB
+    .remove(id)
+    .then(deleteAction => {
+        res.status(200).json(deleteAction)
+    })
+    .catch(error => {
+        res.status(500).json({errorMessage: `${error} could not delete your request`})
+})
+})
 module.exports = router
